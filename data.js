c8d60f4473fff32dcf903ad974d0da038fca9f10
Dolphin <167563184+austinkden@users.noreply.github.com>
2024-07-25 11:12:08 -0600

    fixed working multifamily

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 const incidentData = [
     { "Incident": "Reported Structure Fire", "Response": "1BC 1SAF 2E 1ARL 1M OPS" },
     { "Incident": "Working Residential Fire", "Response": "4E 1ARL 2M 2BC 1SAF 1MED BU OPS" },
-    { "Incident": "Working Multi-Family Fire", "Resposne": "4E 2ARL 2M 2BC 1SAF 1MED BU PIO IDT RH OPS" },
+    { "Incident": "Working Multi-Family Fire", "Response": "4E 2ARL 2M 2BC 1SAF 1MED BU PIO IDT RH OPS" },
     { "Incident": "2nd Alarm Wildland Interface Fire", "Response": "(+) 3E 3BR 3TN 1M 1DC 1BC 1SAF OPS14" }
 ];
