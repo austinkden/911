533845a16093c6a170ec99230b6911da9267fd28
Dolphin <167563184+austinkden@users.noreply.github.com>
2024-07-24 22:27:03 -0600

    week

--- data.js
+++ data.js
@@ -4,5 +4,6 @@
 
 const incidentData = [
     { "Incident": "Reported Structure Fire", "Response": "1BC 1SAF 2E 1ARL 1M OPS" },
-    { "Incident": ""}
+    { "Incident": "Working Residential Fire", "Response": "4E 1ARL 2M 2BC 1SAF 1MED BU OPS" },
+    
 ];
