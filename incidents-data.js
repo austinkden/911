b0b4134ad34f20350d5634c781a8836bca4ae96c
Dolphin <167563184+austinkden@users.noreply.github.com>
2024-07-25 13:37:10 -0600

    added a comma!

--- incidents-data.js
+++ incidents-data.js
@@ -1,3 +1,3 @@
 const incidentData = [
-    { "value": "2ndalarmbrushfire", "Incident": "2nd Alarm Brush Fire" "Response": "(+) 1BRE 4BR 2TN 2BC 1SAF OPS14" },
+    { "value": "2ndalarmbrushfire", "Incident": "2nd Alarm Brush Fire", "Response": "(+) 1BRE 4BR 2TN 2BC 1SAF OPS14" },
 ];
