45fe9a56645f2f32ef475aa9d46874e77e9a92c4
Dolphin <167563184+austinkden@users.noreply.github.com>
2024-07-25 13:45:07 -0600

    custom script.js 20-22 attempt

--- incidents-script.js
+++ incidents-script.js
@@ -1,19 +1,22 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var button = document.getElementById("generate");
-    var select = document.getElementById("options");
-    var unitsDisplay = document.querySelector(".units p");
+var generateButton = document.getElementById("generate");
+var importButton = document.getElementById("import")
+var select = document.getElementById("options");
+var unitInput = document.getElementById("units-input")
+var unitsDisplay = document.querySelector(".units p");
 
-    button.addEventListener("click", function() {
-        var selectedOption = select.value;
-
-        var incident = incidentData.find(function(incident) {
-            return incident.value === selectedOption;
-        });
-
-        if (incident) {
-            unitsDisplay.textContent = incident.Response;
-        } else {
-            unitsDisplay.textContent = "Please select a valid incident.";
-        }
+generateButton.addEventListener("click", function() {
+    var selectedOption = select.value;
+    var incident = incidentData.find(function(incident) {
+        return incident.value === selectedOption;
     });
+
+    if (incident) {
+        unitsDisplay.textContent = incident.Response;
+    } else {
+        unitsDisplay.textContent = "Please select a valid incident.";
+    }
+});
+
+importButton.addEventListener("click", function() {
+    unitsDisplay.textContent = unitInput;
 });
