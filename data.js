28dea4388860ea67232fa4f0c3ce68b90bef8187
Dolphin <167563184+austinkden@users.noreply.github.com>
2024-07-25 09:47:46 -0600

    chatgpt arrow 3

--- data.js
+++ data.js
@@ -1,7 +1,3 @@
-
-
-// above is example of format
-
 const incidentData = [
     { "Incident": "Reported Structure Fire", "Response": "1BC 1SAF 2E 1ARL 1M OPS" },
     { "Incident": "Working Residential Fire", "Response": "4E 1ARL 2M 2BC 1SAF 1MED BU OPS" },
