7ee3bfd1d2233eb8f57a05f77b3aad78fa445b3d
Dolphin <167563184+austinkden@users.noreply.github.com>
2024-07-25 14:46:58 -0600

    adding a little more data

--- incidents-data.js
+++ incidents-data.js
@@ -1,3 +1,7 @@
 const incidentData = [
     { "value": "2ndalarmbrushfire", "Incident": "2nd Alarm Brush Fire", "Response": "(+) 1BRE 4BR 2TN 2BC 1SAF OPS14" },
+    { "value": "2ndalarmstructurefire", "Incident": "2nd Alarm Structure Fire", "Response": "(+) 4E 1ARL 1R 2M 2BC 1SAF OPS14" },
+    { "value": "2ndalarmwildlandinterfacefire", "Incident": "2nd Alarm Wildland Interface Fire", "Response": "(+) 3E 3BR 3TN 1M 1DC 1BC 1SAF OPS14" },
+    { "value": "airalert2", "Incident": "Air Alert 2", "Response": "RED1 RED2 RED3 1M 1BC 1SAF KAPAOPS" },
+    { "value": "airalert3", "Incident": "Air Alert 3", "Response": "RED1 RED2 RED3 2E 2M 1BC 1SAF 1MED PIO KAPAOPS" },
 ];
