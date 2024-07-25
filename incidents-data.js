0eb5778f032317db1cdbd387d7306ae04d8cc093
Dolphin <167563184+austinkden@users.noreply.github.com>
2024-07-25 11:27:47 -0600

    data update

--- incidents-data.js
+++ incidents-data.js
@@ -1,5 +1,8 @@
 const incidentData = [
-    { "Incident": "2nd Alarm Brush Fire", "Response": "(+) 1BRE 4BR 2TN 2BC 1SAF OPS14" }
-    { "Incident": "2nd Alarm Structure Fire", "Response": "(+) 4E 1ARL 1R 2M 2BC 1SAF OPS14" }
-    { "Incident": "2nd Alarm Wildland Interface Fire", "Response": "(+) 3E 3BR 3TN 1M 1DC 1BC 1SAF OPS14" }
+    { "Incident": "2nd Alarm Brush Fire", "Response": "(+) 1BRE 4BR 2TN 2BC 1SAF OPS14" },
+    { "Incident": "2nd Alarm Structure Fire", "Response": "(+) 4E 1ARL 1R 2M 2BC 1SAF OPS14" },
+    { "Incident": "2nd Alarm Wildland Interface Fire", "Response": "(+) 3E 3BR 3TN 1M 1DC 1BC 1SAF OPS14" },
+    { "Incident": "Air Alert 2", "Response": "RED1 RED2 RED3 1M 1BC 1SAF KAPAOPS" },
+    { "Incident": "Air Alert 3", "Response": "RED1 RED2 RED3 2E 2M 1BC 1SAF 1MED PIO KAPAOPS" },
+    { "Incident": "Air Alert 3 Off-Airport", "Response": "1RED 1ARL 2E 2M 2BC 1SAF 1MED PIO OPS" }
 ];
