a56eaeda65d9621db6f3e6b8060bbcd850227d5d
Dolphin <167563184+austinkden@users.noreply.github.com>
2024-07-25 11:59:20 -0600

    fixes

--- incidents-data.js
+++ incidents-data.js
@@ -1,15 +1,3 @@
 const incidentData = [
     { "value": "2ndalarmbrushfire", "Incident": "2nd Alarm Brush Fire", "Response": "(+) 1BRE 4BR 2TN 2BC 1SAF OPS14" },
-    { "value": "2ndalarmstructurefire", "Incident": "2nd Alarm Structure Fire", "Response": "(+) 4E 1ARL 1R 2M 2BC 1SAF OPS14" },
-    { "value": "2ndalarmwildlandinterfacefire", "Incident": "2nd Alarm Wildland Interface Fire", "Response": "(+) 3E 3BR 3TN 1M 1DC 1BC 1SAF OPS14" },
-    { "value": "airalert2", "Incident": "Air Alert 2", "Response": "RED1 RED2 RED3 1M 1BC 1SAF KAPAOPS" },
-    { "value": "airalert3", "Incident": "Air Alert 3", "Response": "RED1 RED2 RED3 2E 2M 1BC 1SAF 1MED PIO KAPAOPS" },
-    { "value": "airalert3offairport", "Incident": "Air Alert 3 Off-Airport", "Response": "1RED 1ARL 2E 2M 2BC 1SAF 1MED PIO OPS" },
-    { "value": "alarmreset", "Incident": "Alarm Reset", "Response": "1SPN" },
-    { "value": "carbonmonoxidealarmnosickparties", "Incident": "Carbon Monoxide Alarm, No Sick Parties", "Response": "1SPN" },
-    { "value": "residentialfirealarm", "Incident": "Residential Fire Alarm", "Response": "1SPN" },
-    { "value": "commercialfirealarm", "Incident": "Commercial Fire Alarm", "Response": "1SPN" },
-    { "value": "corezero", "Incident": "Core Zero", "Response": "Core Zero Response" },
-    { "value": "gunshotwound", "Incident": "Gunshot Wound", "Response": "Gunshot Wound Response" },
-    { "value": "penetratingtrauma", "Incident": "Penetrating Trauma", "Response": "Penetrating Trauma Response" }
 ];
