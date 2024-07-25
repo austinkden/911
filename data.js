c24b72798160d252db93bb975e5b1525ee5c71a7
Dolphin <167563184+austinkden@users.noreply.github.com>
2024-07-25 11:24:38 -0600

    data udpate

--- data.js
+++ data.js
@@ -1,6 +1,5 @@
 const incidentData = [
-    { "Incident": "Reported Structure Fire", "Response": "1BC 1SAF 2E 1ARL 1M OPS" },
-    { "Incident": "Working Residential Fire", "Response": "4E 1ARL 2M 2BC 1SAF 1MED BU OPS" },
-    { "Incident": "Working Multi-Family Fire", "Response": "4E 2ARL 2M 2BC 1SAF 1MED BU PIO IDT RH OPS" },
+    { "Incident": "2nd Alarm Brush Fire", "Response": "(+) 1BRE 4BR 2TN 2BC 1SAF OPS14" }
+    { "Incident": "2nd Alarm Structure Fire", "Response": "(+) 4E 1ARL 1R 2M 2BC 1SAF OPS14" }
     { "Incident": "2nd Alarm Wildland Interface Fire", "Response": "(+) 3E 3BR 3TN 1M 1DC 1BC 1SAF OPS14" }
 ];
